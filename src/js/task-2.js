ffcd0a76bf718d57c4cef632bf5a6d2aaf30e383
Sergei Sharifov <2017ok@gmail.com>
2020-06-12 22:49:19 +0200

    Modul-04

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,35 +1,31 @@
-"use strict" /* использование последней  версии  js  */;
+'use strict' /* использование последней  версии  js  */;
 
 const inventory = {
-  items: ["Knife", "Gas mask"],
-
+  items: ['Knife', 'Gas mask'],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
-    console.log(itemName);
-
-    inventory.items.push(itemName);
+    this.items.push(itemName);
   },
-
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    inventory.items = inventory.items.filter((item) => item !== itemName);
+    this.items = this.items.filter(item => item !== itemName);
   },
 };
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+  action.call(inventory, itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction('Medkit', inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
